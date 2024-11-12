8904664e0e4893643323364ccb3330650bd151d3
Nhat Dang Minh <nhat.dang2004.cv@gmail.com>
2024-11-12 21:10:26 +0700

    Fix layout + pricing page

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { store } from "@/store";
 import ProviderWrapper from "@/hooks";
 import { getMessages } from "next-intl/server";
+import PublicLayout from "@/layouts/publicLayout";
 import { NextIntlClientProvider } from "next-intl";
-import React from "react";
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const messages = await getMessages();
@@ -11,7 +12,9 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
   return (
     <NextIntlClientProvider messages={messages} locale={locale}>
       <ProviderWrapper>
-        {children}
+        <PublicLayout>
+          {children}
+        </PublicLayout>
       </ProviderWrapper>
     </NextIntlClientProvider>
   );
