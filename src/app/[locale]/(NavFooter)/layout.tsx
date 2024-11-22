52306be12e2379ff09d8895a029b4170664f498b
QuangTran013 <113117168+daVinc3nt@users.noreply.github.com>
2024-11-22 15:44:06 +0700

    sửa lại layout cuảgroup

---
+++ src/app/[locale]/(NavFooter)/layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { store } from "@/store";
+import ProviderWrapper from "@/hooks";
+import { getMessages } from "next-intl/server";
+import PublicLayout from "@/layouts/publicLayout";
+import { NextIntlClientProvider } from "next-intl";
+
+export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const messages = await getMessages();
+  const locale = store.getState().language.locale;
+
+  return (
+    <NextIntlClientProvider messages={messages} locale={locale}>
+      <ProviderWrapper>
+        <PublicLayout>
+          {children}
+        </PublicLayout>
+      </ProviderWrapper>
+    </NextIntlClientProvider>
+  );
+};
