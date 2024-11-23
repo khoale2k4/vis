931325176b47b4ce1e768a5e67d32936068db1c9
QuangTran013 <113117168+daVinc3nt@users.noreply.github.com>
2024-11-23 19:15:11 +0700

    ok

--- src/views/authentication/authTemplate.tsx
+++ src/views/authentication/authTemplate.tsx
@@ -3,15 +3,33 @@ import LoginForm from "./form/login";
 import RegisterForm from "./form/register";
 import { ResetForm } from "./form/forgotPassword";
 import { useState } from "react";
+import Image from "next/image";
 export default function AuthView() {
     const [view, setView] = useState<'login' | 'register' | 'reset'>('login');
     return (
-        <div className="w-screen h-screen grid place-items-center bg-neutral-10">
-            <div className="w-1/3 ">
-                {view === 'login' && < LoginForm setView={setView} />}
-                {view === 'register' && < RegisterForm setView={setView} />}
-                {view === 'reset' && < ResetForm setView={setView} />}
-            </div>
-        </div>
+        <div className=" bg-lightContainerPrimary w-screen h-screen grid place-items-center">
+			<div className="w-full h-32 rounded-full relative block lg:hidden">
+				<Image
+					src="/authentication/AlphaSolutions.webp"
+					alt="AlphaSolutions"
+					fill
+					className="w-full h-full  top-0 left-0 object-cover"
+				/>
+			</div>
+			<div className=" grid place-items-center lg:w-1/3 w-full">
+				{view === 'login' && < LoginForm setView={setView} />}
+				{view === 'register' && < RegisterForm setView={setView} />}
+				{view === 'reset' && < ResetForm setView={setView} />}
+			</div>
+			{/* <div className="w-1/2 h-full relative hidden lg:block">
+				<Image
+					src="/authentication/AlphaSolutions.webp"
+					alt="AlphaSolutions"
+					fill
+					className="w-full h-full top-0 left-0 object-cover"
+				/>
+			</div> */}
+		</div>
+
     );
 }
